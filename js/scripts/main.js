4129cad79f273d7946c952adfefd34621ecb973f
Jhonata Nogueira <jhoownogueira@outlook.com>
2022-07-11 22:12:20 -0300

    swiper slide lib
    Slider principal e cards

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -1,3 +1,26 @@
+var swiper = new Swiper(".content-slides", {
+    spaceBetween: 30,
+    effect: "fade",
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  var swiper = new Swiper(".slider-videos", {
+    slidesPerView: 5,
+    spaceBetween: 32,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+
 const menuHamburguer = document.querySelector('.menu-hamburguer');
 const btnHamburguer = document.getElementById('btn-hamburguer');
 
