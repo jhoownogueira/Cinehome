584682ceb721c095d4040a4d7cc482b9ac8e450f
Jhonata Nogueira <jhoownogueira@outlook.com>
2022-07-19 22:02:53 -0300

    Slide Trending Dinâmico

--- js/scripts/home_axios.js
+++ js/scripts/home_axios.js
@@ -48,7 +48,6 @@ axios({
 // Home - Filmes Populares
     // Create Card By Api
 const areaFilmesPopulares = document.getElementById('filmes-populares-home');
-console.log(areaFilmesPopulares)
 
 function createCardFilmesPopulares(nome, ano, imagem) {
     let swiper = document.createElement('div');
@@ -90,5 +89,56 @@ axios({
         }
 
         createCardFilmesPopulares(infoCard.nome, infoCard.ano, infoCard.image);
+    })
+})
+
+const areaSlideTrending = document.getElementById('trending-home');
+console.log(areaSlideTrending)
+
+function createSlideTrending(nome, info, image) {
+    let swiper = document.createElement('div');
+    swiper.classList = 'swiper-slide';
+    swiper.style.backgroundImage = `url('https://image.tmdb.org/t/p/original/${image}')`;
+    areaSlideTrending.appendChild(swiper);
+    let titulo = document.createElement('h1');
+    titulo.innerText = `${nome}`;
+    swiper.appendChild(titulo);
+    let sinopse = document.createElement('p');
+    sinopse.innerText = `${info}`;
+    swiper.appendChild(sinopse);
+    let buttonsDivSlide = document.createElement('div');
+    buttonsDivSlide.classList = 'buttons-slides';
+    swiper.appendChild(buttonsDivSlide);
+    let buttonsDiv = document.createElement('div');
+    buttonsDiv.classList = 'buttons';
+    buttonsDivSlide.appendChild(buttonsDiv);
+    let buttonsAssista = document.createElement('button');
+    buttonsAssista.classList = 'primary-button';
+    buttonsAssista.innerText = 'Assista agora';
+    buttonsDiv.appendChild(buttonsAssista);
+    let buttonsAddList = document.createElement('button');
+    buttonsAddList.classList = 'secondary-button';
+    buttonsAddList.innerHTML = '<img src="img/icon-favoritar.svg" alt="">';
+    buttonsDiv.appendChild(buttonsAddList);
+
+}
+
+axios({
+    method: 'GET',
+    url: 'https://api.themoviedb.org/3/trending/movie/day?api_key=93f7813158e109c144b2cfefacb802be&language=pt-BR',
+})
+.then(json => {
+
+    const {results} = json.data;
+    results.forEach(index => {
+
+        const infoSlide = {
+            nome: index.title,
+            info: index.overview,
+            image: index.poster_path,
+        }
+
+        createSlideTrending(infoSlide.nome, infoSlide.info, infoSlide.image);
+
     })
 })
