711d46a52b487f9ed8e41dcfebbd15f45886b392
Jhonata Nogueira <jhoownogueira@outlook.com>
2022-07-19 21:09:09 -0300

    Sliders SwiperJs

---
+++ js/scripts/sliders.js
@@ -0,0 +1,25 @@
+var swiper = new Swiper(".content-slides", {
+    observer: true,
+    observeParents: true,
+    spaceBetween: 30,
+    effect: "fade",
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  var swiper = new Swiper(".slider-videos", {
+    observer: true,
+    observeParents: true,
+    slidesPerView: 5,
+    spaceBetween: 32,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
