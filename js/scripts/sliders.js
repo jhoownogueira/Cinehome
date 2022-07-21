d62cd052d98e4783f179e8365b3ba586069498d8
Jhonata Nogueira <jhoownogueira@outlook.com>
2022-07-21 20:48:25 -0300

    Responsive Sliders

--- js/scripts/sliders.js
+++ js/scripts/sliders.js
@@ -1,26 +1,53 @@
+
 var swiper = new Swiper(".content-slides", {
     observer: true,
     observeParents: true,
-    loop: true,
-    spaceBetween: 30,
-    effect: "fade",
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
     },
+    effect: "fade",
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
+    loop: true,
   });
 
   var swiper = new Swiper(".slider-videos", {
     observer: true,
     observeParents: true,
-    slidesPerView: 5,
-    spaceBetween: 32,
+    slidesPerView: 1.1,
+    spaceBetween: 0,
+    loop: true,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
+    },
+    breakpoints: {
+      600: {
+        slidesPerView: 1.8,
+        spaceBetween: 16,
+      },
+      800: {
+        slidesPerView: 2.2,
+        spaceBetween: 8,
+      },
+      1024: {
+        slidesPerView: 3.5,
+        spaceBetween: 16,
+      },
+      1278: {
+        slidesPerView: 4.2,
+        spaceBetween: 16,
+      },
+      1366: {
+        slidesPerView: 4.8,
+        spaceBetween: 16,
+      },
+      1512: {
+        slidesPerView: 7.5,
+        spaceBetween: 0,
+      },
     },
   });
